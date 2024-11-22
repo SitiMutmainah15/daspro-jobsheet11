13c261a43addd795e3e0f4038f3ad5c1667f08fa
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2024-11-22 20:20:50 +0700

    coba2

--- Kafe21.java
+++ Kafe21.java
@@ -1,8 +1,13 @@
 public class Kafe21 {
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
-    public static void Menu () {
+    public static void Menu (String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
 
     System.out.println("==== MENU RESTO KAFE ====");
     System.out.println("1. Kopi Hitam - Rp 15,000");
