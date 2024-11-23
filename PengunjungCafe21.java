ac1131724dd0f9b4d7f074bde3ff53c9a8233d76
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2024-11-23 22:14:17 +0700

    coba4

---
+++ PengunjungCafe21.java
@@ -0,0 +1,11 @@
+public class PengunjungCafe21 {
+    public static void main(String[] args) {
+       daftarPengunjung("Ali", "Budi", "Citra");
+    }
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar nama pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+}
